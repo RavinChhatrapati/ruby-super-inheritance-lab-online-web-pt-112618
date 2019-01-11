52eb063d8b85fd55373483a32c9600ae5de926ef
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-01-11 01:57:09 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,8 +4,8 @@ super
 puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 end
 def raise_hand
-  10.times do
-    super
+  10.times do   takes the method raise_hand and outputs it 10 times 
+    super          super changes the raise_hand to pick me ten times 
   end
 end
 end
